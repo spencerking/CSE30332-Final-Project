41015fcd9e8704ff98003d8331b58d2599d56215
spencerking <sking8@nd.edu>
2015-04-23 20:01:23 -0400

    loading different tank type images

--- tank.py
+++ tank.py
@@ -7,9 +7,17 @@ class Tank(pygame.sprite.Sprite):
 	def __init__(self, gs=None):
 		pygame.sprite.Sprite.__init__(self)
 
+		self.dir_str = None
 		#pass the tank type to determine which image to use
-		self.tank_image = pygame.image.load('tank/tank0.bmp')
-		self.turret_image = pygame.image.load('tank/turret0.bmp')
+		if type == 1:
+			self.dir_str = 'green'
+		elif type == 2:
+			self.dir_str = 'blue'
+		elif type == 3:
+			self.dir_str = 'red'
+
+		self.tank_image = pygame.image.load('tank/' + self.dir_str + '/tank0.bmp')
+		self.turret_image = pygame.image.load('tank/' + self.dir_str + '/tank0.bmp')
 
 		#look into masking instead
 		self.rect = self.image.get_rect()
