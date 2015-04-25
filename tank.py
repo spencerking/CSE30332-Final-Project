7b947898c631913ba43e574b0b63c43f8afa0a5d
spencerking <sking8@nd.edu>
2015-04-25 18:58:27 -0400

    more tank.py changes

--- tank.py
+++ tank.py
@@ -8,7 +8,15 @@ class Tank(pygame.sprite.Sprite):
 		pygame.sprite.Sprite.__init__(self)
 
 		self.dir_str = None
-		self.direction = 0 #direction is initially NW, 1 is N, 2 NE, etc.
+
+		#stores which tile the tank is currently on, updates as the tank moves
+		self.curr_tile = [0, 0]
+
+		#direction is initially NW, 1 is N, 2 NE, etc.
+		self.direction = 0
+		self.px = 0
+		self.py = 0
+		self.f_angle = 0
 
 		'''
 				NW
@@ -36,7 +44,6 @@ SW									NE
 			self.health = 75
 			self.strength = 15
 
-		self.direction = 0
 		self.tank_image = pygame.image.load('tank/' + self.dir_str + '/tank%d.bmp' % self.direction)
 		self.turret_image = pygame.image.load('tank/' + self.dir_str + '/tank%d.bmp' % self.direction)
 
@@ -54,44 +61,77 @@ SW									NE
 		mx, my = pygame.mouse.get_pos()
 
 		#probably reuse the rotation stuff from the pygame primer here
+		rot_angle = 360 - math.atan2(my-self.py, mx-self.px)*180/math.pi
+		fire_angle = math.atan2(my-self.py, mx-self.px)
+		rot_img = pygame.transform.rotate(self.orig_turret, rot_angle)
+		self.turret_image = self.rot_img
+		self.f_angle = fire_angle
 
 	def move(self, orientation):
 		#MOVING THE TANK FORWARD
 		if orientation == "forward":
 			if self.direction == 0:
 				self.rect = self.rect.move(0,-45)
+				py -= 45
 			elif self.direction == 1:
 				self.rect = self.rect.move(91,-45)
+				px += 91
+				py -= 45
 			elif self.direction == 2:
 				self.rect = self.rect.move(91,0)
+				px += 91
 			elif self.direction == 3:
 				self.rect = self.rect.move(91,45)
+				px += 91
+				py += 45
 			elif self.direction == 4:
 				self.rect = self.rect.move(0,45)
+				py += 45
 			elif self.direction == 5:
 				self.rect = self.rect.move(-91,45)
+				px -= 91
+				py += 45
 			elif self.direction == 6:
 				self.rect = self.rect.move(-91,0)
+				px -= 91
 			elif self.direction == 7:
 				self.rect = self.rect.move(-91,-45)
+				px -= 91
+				py -= 45
 		#MOVING THE TANK BACKWARD
 		elif orientation == "backward":
 			if self.direction == 0:
 				self.rect = self.rect.move(0,45)
+				py += 45
 			elif self.direction == 1:
 				self.rect = self.rect.move(-91,45)
+				px -= 91
+				py += 45
 			elif self.direction == 2:
 				self.rect = self.rect.move(-91,0)
+				px -= 91
 			elif self.direction == 3:
 				self.rect = self.rect.move(-91,-45)
+				px -= 91
+				py -= 45
 			elif self.direction == 4:
 				self.rect = self.rect.move(0,-45)
+				py -= 45
 			elif self.direction == 5:
 				self.rect = self.rect.move(91,-45)
+				px += 91
+				py -= 45
 			elif self.direction == 6:
 				self.rect = self.rect.move(91,0)
+				px += 91
 			elif self.direction == 7:
 				self.rect = self.rect.move(91,45)
+				px += 91
+				py += 45
+
+	#fires a bullet
+	def fire(self):
+		self.fire_sound.play()
 
 	def key_handler(self, keycode):
 		#8 directions
@@ -108,5 +148,27 @@ SW									NE
 			self.direction -= 1
 			if self.direction < 0:
 				self.direction = 7
+		elif keycode == K_SPACE:
+			self.fire()
 			
 		return
+
+
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, angle, x, y, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load("laser.png")
+		self.rect = self.image.get_rect()
+
+		self.l_start_x = x
+		self.l_start_y = y
+		self.rect.centerx = self.l_start_x
+		self.rect.centery = self.l_start_y
+		self.l_angle = angle
+		self.speed = 3
+
+	def tick(self):
+		l_x_pos = self.speed*math.cos(self.l_angle)
+		l_y_pos = self.speed*math.sin(self.l_angle)
+		self.rect.centerx = self.rect.centerx + l_x_pos
+		self.rect.centery = self.rect.centery + l_y_pos
