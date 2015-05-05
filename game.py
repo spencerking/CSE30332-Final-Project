5ccb71643bab37a21ff2f3542bb79130b644951e
spencerking <sking8@nd.edu>
2015-05-04 20:02:55 -0400

    random start points working, random colors not working

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ from world import World
 from tank import Tank, Bullet
 from player import Player
 from enemy import Enemy
+from random import randint
 
 class GameSpace:
     def main(self):
@@ -22,12 +23,57 @@ class GameSpace:
 
         self.world = World(7, 7, self)
         self.sprites.append(self.world)
-        # TODO: check if tank location is not water
-        self.player = Player('green', (0,0), self)
-        self.sprites.append(self.player)
-        self.enemy = Enemy('blue', (5,5), self)
-        self.sprites.append(self.enemy)
 
+
+        self.test = Player("red", (0,0), self)
+        self.sprites.append(self.test)
+        
+        #randomly position tanks and make sure the position is valid
+        x = 1
+        while x == 1: 
+            p_randx = randint(0,6)
+            p_randy = randint(0,6)
+            if self.test.check_tile(p_randx, p_randy):
+                 x = 2
+
+        while x == 2: 
+            e_randx = randint(0,6)
+            e_randy = randint(0,6)
+            if self.test.check_tile(e_randx, e_randy):
+                 x = 3
+
+        self.sprites.remove(self.test)
+
+        #instantiate tanks with random colors
+        '''
+        rand_color = randint(1,3)
+        if rand_color == 1:
+            self.player = Player('green', (p_randx,p_randy), self)
+            self.sprites.append(self.player)
+        elif rand_color == 2:
+            self.player = Player('red', (p_randx,p_randy), self)
+            self.sprites.append(self.player)
+        elif rand_color == 3:
+            self.player = Player('blue', (p_randx,p_randy), self)
+            self.sprites.append(self.player)
+
+        rand_color = randint(1,3)
+        if rand_color == 1:
+            self.enemy = Enemy('green', (e_randx,e_randy), self)
+            self.sprites.append(self.enemy)
+        elif rand_color == 2:
+            self.enemy = Enemy('red', (e_randx,e_randy), self)
+            self.sprites.append(self.enemy)
+        elif rand_color == 3:
+            self.enemy = Enemy('blue', (e_randx,e_randy), self)
+            self.sprites.append(self.enemy)
+        '''
+        
+        self.player = Player('green', (p_randx,p_randy), self)
+        self.sprites.append(self.player)
+        self.enemy = Enemy('blue', (e_randx,e_randy), self)
+        self.sprites.append(self.enemy)
+        
         #CHANGE LATER
         self.player.id = 0
         self.enemy.id = 1
